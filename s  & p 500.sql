2d777ddcd12aaead5a517f1288359d02065b20a1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-01 09:38:40 +0300

    final

--- s  & p 500.sql
+++ s  & p 500.sql
@@ -1,3 +1,12 @@
 -- create table and load data --
 CREATE DATABASE snp500
 GO
+
+
+CREATE TABLE CompanyBySector
+(Ticker varchar(5)
+,CompanyName varchar (50)
+,Sector varchar(50)
+,StockPrice decimal(6,2)
+)
+GO
